5fb13b99a89c0fbe534e001816464ca14f2fbfc5
ClecioSousa00 <cleciosouza015@gmail.com>
2025-06-21 15:22:33 -0300

    Feat: add controller register user

---
+++ src/infra/database/prisma/repositories/user-prisma-repository.ts
@@ -0,0 +1,60 @@
+import { UserRepository } from '@/domain/application/repositories/user-repository'
+import { User } from '@/domain/enterprise/entities/user'
+import { Email } from '@/domain/enterprise/value-objects/Email'
+import { prisma } from '@/lib/prisma-client'
+import { UniqueEntityId } from '@/shared/entities/unique-entity-id'
+
+export class UserPrismaRepository implements UserRepository {
+  async create(user: User): Promise<void> {
+    await prisma.user.create({
+      data: {
+        email: user.email,
+        password: user.password,
+        username: user.password,
+        id: user.id.toString(),
+      },
+    })
+  }
+
+  async findByEmail(email: string) {
+    const data = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!data) return null
+
+    const user = User.create(
+      {
+        email: new Email(data.email),
+        password: data.password,
+        username: data.password,
+      },
+      new UniqueEntityId(data.id),
+    )
+
+    return user
+  }
+
+  async findById(id: UniqueEntityId) {
+    const data = await prisma.user.findUnique({
+      where: {
+        id: id.toString(),
+      },
+    })
+
+    if (!data) return null
+
+    const user = User.create(
+      {
+        email: new Email(data.email),
+        password: data.password,
+        username: data.username,
+      },
+      new UniqueEntityId(data.id),
+    )
+
+    return user
+  }
+}
--- src/infra/http/controllers/user/register-user-controller.ts
+++ src/infra/http/controllers/user/register-user-controller.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { z } from 'zod'
 import { validateData } from '../../middleware/validationMiddleware'
+import { UserPrismaRepository } from '@/infra/database/prisma/repositories/user-prisma-repository'
+import { UserAlreadyExistsError } from '@/shared/errors/user-already-exists-error'
+import { RegisterUserUseCase } from '@/domain/application/use-cases/user/register-user-use-case'
 
 const registerBodySchema = z.object({
   username: z.string().min(3),
@@ -13,6 +16,19 @@ export const registerValidation = validateData('body', registerBodySchema)
 
 export async function RegisterUserController(req: Request, res: Response) {
   const { email, password, username } = registerBodySchema.parse(req.body)
+
+  try {
+    const userRepository = new UserPrismaRepository()
+    const registerUseCase = new RegisterUserUseCase(userRepository)
+
+    await registerUseCase.execute({ email, password, username })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      res.status(StatusCodes.CONFLICT).send({
+        message: error.message,
+      })
+    }
+  }
 
   res.status(StatusCodes.OK).json()
 }
