3ee6ffc9cfd8986b8b5613b9282c0779c7e118a4
ClecioSousa00 <cleciosouza015@gmail.com>
2025-07-09 22:13:33 -0300

    Feat: add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
 import express from 'express'
 import { routes } from './infra/http/routes/router'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
+
+const swaggerDocs = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Manager Products',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/infra/http/routes/router.ts'],
+})
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use(express.json())
 app.use(routes)
